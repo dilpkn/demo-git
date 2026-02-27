b8a2edd01e0b6628ac0bccc85ac8d6e09f1fa01d
dilpkn <dianawb19@gmail.com>
2026-02-27 19:09:23 +0300

    test commit-2

--- src/main/java/org/example/demogit2/DemoGit2Application.java
+++ src/main/java/org/example/demogit2/DemoGit2Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoGit2Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
         SpringApplication.run(DemoGit2Application.class, args);
     }
 
