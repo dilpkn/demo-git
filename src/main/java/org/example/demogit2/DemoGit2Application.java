0a7fbd48a79a12c049bba1310ad904e579ddc6a6
dilpkn <dianawb19@gmail.com>
2026-02-27 16:23:32 +0300

    init commit

---
+++ src/main/java/org/example/demogit2/DemoGit2Application.java
@@ -0,0 +1,13 @@
+package org.example.demogit2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGit2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGit2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/demogit2/DemoGit2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demogit2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGit2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
